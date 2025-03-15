fd455595afbf447f551f1d0b6414df9fbb26a52a
Leonv <leon.vandenberghe@ugent.be>
2025-03-15 11:32:36 +0100

    Stage releases background resources

--- src/main/java/be/ugent/objprog/minionwars/controllers/GameController.java
+++ src/main/java/be/ugent/objprog/minionwars/controllers/GameController.java
@@ -5,6 +5,7 @@ import be.ugent.objprog.minionwars.models.TileModel;
 import be.ugent.objprog.minionwars.views.GameView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -14,19 +15,28 @@ public class GameController {
     private GameView view;
     private PlayerModel playerModel;
     private TileModel tileModel;
+    private Stage stage;
 
-    public GameController(PlayerModel playerModel,Locale locale) {
+    public GameController(Stage stage,PlayerModel playerModel, Locale locale) {
+        this.stage = stage;
         this.playerModel = playerModel;
         this.tileModel = new TileModel();
         this.locale = locale;
         this.view = new GameView(playerModel,tileModel,locale);
         view.resetGameGroupPosition();
 
-
+        view.getEndTurnButton().setOnAction(event -> {
+            endGame();
+        });
+        stage.setOnCloseRequest(event -> {
+            view.getGameTileGroupPane().shutdown();// Releases resources from other threads
+        });
     }
     public void endGame(){
         //TODO launch new game
-        view.getGameTileGroupPane().shutdown(); // Releases resources from other threads
+        view.getGameTileGroupPane().shutdown();
+        stage.close();
+
     }
     public Region getView() {
         return this.view.getView();
