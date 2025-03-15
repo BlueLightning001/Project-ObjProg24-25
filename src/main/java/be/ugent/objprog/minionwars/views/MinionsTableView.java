f6fce81547b5bbd043b6b0f25fe98f3d966a7a8f
Leonv <leon.vandenberghe@ugent.be>
2025-03-15 18:34:24 +0100

    MenuTable scales properly

--- src/main/java/be/ugent/objprog/minionwars/views/MinionsTableView.java
+++ src/main/java/be/ugent/objprog/minionwars/views/MinionsTableView.java
@@ -1,22 +1,26 @@
 package be.ugent.objprog.minionwars.views;
 
-import be.ugent.objprog.minionwars.InfoCircleExample;
 import be.ugent.objprog.minionwars.minions.Minion;
-import be.ugent.objprog.minionwars.minions.MinionTypeImage;
 import be.ugent.objprog.minionwars.models.MinionModel;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+import java.util.Objects;
 
 public class MinionsTableView extends TableView<Minion> {
     public MinionsTableView(MinionModel model) {
@@ -30,49 +34,120 @@ public class MinionsTableView extends TableView<Minion> {
 
 
         TableColumn<Minion, String> nameCol = new TableColumn<>();
-        nameCol.setCellValueFactory(cell ->{
+        nameCol.setCellValueFactory(cell -> {
             return new SimpleStringProperty(cell.getValue().getName());
         });
+        nameCol.setCellFactory(column -> new TableCell<Minion, String>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null || item.isBlank()) {
+                    setGraphic(null);
+                    setText(null);
+                } else {
+                    setText(item);
+
+                    // Dynamically adjust font size
+                    ChangeListener<Number> resizeListener = (obs, oldSize, newSize) -> updateFontSize();
+                    
+                    widthProperty().addListener(resizeListener);
+                    tableRowProperty().addListener((obs, oldRow, newRow) -> {
+                        if (newRow != null) {
+                            newRow.heightProperty().addListener(resizeListener);
+                        }
+                    });
+
+                    updateFontSize();
+                    setAlignment(Pos.CENTER);
+                    setWrapText(true);
+
+                }
+            }
+
+
+            private void updateFontSize() {
+                TableRow<Minion> row = getTableRow();
+                if (row != null) {
+                    double fontSize = Math.min(getWidth() * 0.12, row.getHeight() * 0.5); // Scale factor
+                    setFont(Font.font("Monotype Corsiva", FontWeight.EXTRA_BOLD, fontSize));
+                }
+            }
+        });
+
 
         TableColumn<Minion, VBox> statsCol = new TableColumn<>();
-        // Cell value factory (returns empty property, since we don't use it directly)
+        // (returns empty property, since we don't use it directly)
         statsCol.setCellValueFactory(cellData -> new SimpleObjectProperty<>(null));
 
 
-
-        // Cell factory to create the VBox dynamically
-        statsCol.setCellFactory(column -> new TableCell<Minion, VBox>() {
+        statsCol.setCellFactory(column -> new TableCell<>() {
             @Override
             protected void updateItem(VBox vbox, boolean empty) {
                 super.updateItem(vbox, empty);
-
+                double iconScale = 0.2;
+                double fontScale = 0.15;
                 Minion minion = getTableRow() != null ? getTableRow().getItem() : null;
 
                 if (empty || minion == null) {
                     setGraphic(null);
                 } else {
+                    // Price label
                     Label priceLabel = new Label();
-                    priceLabel.textProperty().bind(Bindings.format("Cost: %d", minion.costProperty()));
+                    priceLabel.textProperty().bind(Bindings.convert(minion.costProperty()));
+                    ImageView priceImageView = new ImageView(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/be/ugent/objprog/minionwars/images/icons/coin-FFB900.png"))));
+                    priceLabel.setGraphic(priceImageView);
 
+                    // Scaling
+                    priceImageView.setPreserveRatio(true);
+                    priceImageView.fitHeightProperty().bind(getTableColumn().widthProperty().multiply(iconScale));
+                    priceImageView.fitWidthProperty().bind(priceImageView.fitHeightProperty());
+                    priceLabel.styleProperty().bind(
+                            Bindings.format("-fx-font-size: %.2fpx;", getTableColumn().widthProperty().multiply(fontScale))
+                    );
+
+                    // Attack label
                     Label attackLabel = new Label();
-                    attackLabel.textProperty().bind(Bindings.format("Attack: %d", minion.attackProperty()));
+                    attackLabel.textProperty().bind(Bindings.convert(minion.attackProperty()));
+                    ImageView attackImageView = new ImageView(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/be/ugent/objprog/minionwars/images/icons/attack-D60000.png"))));
+                    attackLabel.setGraphic(attackImageView);
 
+
+                    attackImageView.setPreserveRatio(true);
+                    attackImageView.fitHeightProperty().bind(getTableColumn().widthProperty().multiply(iconScale));
+                    attackImageView.fitWidthProperty().bind(attackImageView.fitHeightProperty());
+                    attackLabel.styleProperty().bind(
+                            Bindings.format("-fx-font-size: %.2fpx;", getTableColumn().widthProperty().multiply(fontScale))
+                    );
+
+                    // Defense label
                     Label defenseLabel = new Label();
-                    defenseLabel.textProperty().bind(Bindings.format("Defense: %d", minion.defenceProperty()));
+                    defenseLabel.textProperty().bind(Bindings.convert(minion.defenceProperty()));
+                    ImageView defenseImageView = new ImageView(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/be/ugent/objprog/minionwars/images/icons/health-D60000.png"))));
+                    defenseLabel.setGraphic(defenseImageView);
+
+
+                    defenseImageView.setPreserveRatio(true);
+                    defenseImageView.fitHeightProperty().bind(getTableColumn().widthProperty().multiply(iconScale));
+                    defenseImageView.fitWidthProperty().bind(defenseImageView.fitHeightProperty());
+                    defenseLabel.styleProperty().bind(
+                            Bindings.format("-fx-font-size: %.2fpx;", getTableColumn().widthProperty().multiply(fontScale))
+                    );
+
 
                     VBox statsVBox = new VBox(5, priceLabel, attackLabel, defenseLabel);
                     statsVBox.setAlignment(Pos.CENTER_LEFT);
 
                     setGraphic(statsVBox);
+                    setAlignment(Pos.CENTER_LEFT);
                 }
             }
         });
 
-        getColumns().setAll(minionIconCol,nameCol,statsCol);
+        getColumns().setAll(minionIconCol, nameCol, statsCol);
         // Adjust the column widths to fit the content
         minionIconCol.prefWidthProperty().bind(widthProperty().multiply(0.2));
-        nameCol.prefWidthProperty().bind(widthProperty().multiply(0.6));
-        statsCol.prefWidthProperty().bind(widthProperty().multiply(0.2));
+        nameCol.prefWidthProperty().bind(widthProperty().multiply(0.5));
+        statsCol.prefWidthProperty().bind(widthProperty().multiply(0.3));
 
         getStylesheets().add(MinionsTableView.class.getResource("/be/ugent/objprog/minionwars/css/tableview.css").toExternalForm());
         getStyleClass().add("noheader");
@@ -97,11 +172,12 @@ public class MinionsTableView extends TableView<Minion> {
                     // Creating the circle for the clip
                     Circle circle = new Circle();
                     circle.setFill(new ImagePattern(imageView.getImage()));
-                    circle.radiusProperty().bind(getTableColumn().prefWidthProperty().multiply(0.4));
+                    circle.radiusProperty().bind(getTableColumn().prefWidthProperty().multiply(0.35));
 
                     circle.setCenterY(getTableRow().getHeight() / 2);
                     circle.setCenterX(getTableColumn().getWidth() / 2);
                     setGraphic(circle);
+                    setAlignment(Pos.CENTER);
                 }
             }
         });
