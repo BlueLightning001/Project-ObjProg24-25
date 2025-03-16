f708974a98a139982f16bb1549f58219578d5143
Leonv <leon.vandenberghe@ugent.be>
2025-03-16 13:40:22 +0100

    Able to delete placed minions

--- src/main/java/be/ugent/objprog/minionwars/controllers/GameController.java
+++ src/main/java/be/ugent/objprog/minionwars/controllers/GameController.java
@@ -1,6 +1,7 @@
 package be.ugent.objprog.minionwars.controllers;
 
 import be.ugent.objprog.minionwars.JDOMReader;
+import be.ugent.objprog.minionwars.ZoomableScrollPane;
 import be.ugent.objprog.minionwars.minions.Minion;
 import be.ugent.objprog.minionwars.models.MinionModel;
 import be.ugent.objprog.minionwars.models.Player;
@@ -9,17 +10,13 @@ import be.ugent.objprog.minionwars.models.TileModel;
 import be.ugent.objprog.minionwars.tiles.HexTile;
 import be.ugent.objprog.minionwars.tiles.Tile;
 import be.ugent.objprog.minionwars.views.GameView;
-import javafx.scene.Node;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
 import java.util.Locale;
-import java.util.Objects;
 
 public class GameController {
     private final Locale locale;
@@ -75,17 +72,24 @@ public class GameController {
 
             }
         });
-        view.getGameTileGroupPane().setOnKeyPressed(event -> {
+        getView().setOnKeyPressed(event -> {
             Object eventSource = event.getTarget();
-            if (eventSource instanceof HexTile hexTile && event.getCode() == KeyCode.DELETE ) {
-                Tile tile = hexTile.getTile();
-                Player currentPlayer = playerModel.getCurrentPlayer();
-                Minion occupant = tile.getOccupant();
-                if (tile.isOccupied() && occupant.getOwner().equals(currentPlayer)) {
-                    tile.setOccupant(null);
-                    currentPlayer.addMoney(occupant.getCost());
+            System.out.println(eventSource);
+            if (eventSource instanceof ZoomableScrollPane pane && event.getCode() == KeyCode.DELETE) {
+                HexTile selectedHexTile = view.getGameTileGroupPane().getSelectedHexTile();
+                Tile tileToDelete = null;
+                if (selectedHexTile != null) {
+                    tileToDelete = selectedHexTile.getTile();
+                    Player currentPlayer = playerModel.getCurrentPlayer();
+                    Minion occupant = tileToDelete.getOccupant();
+                    if (tileToDelete.isOccupied() && occupant.getOwner().equals(currentPlayer)) {
+                        tileToDelete.setOccupant(null);
+                        view.getGameTileGroupPane().setSelectedHexTile(null);
+                        currentPlayer.addMoney(occupant.getCost());
 
+                    }
                 }
+
             }
         });
     }
