a55611e567e7f0625ae48aeedf7a4e049d9fd178
Leonv <leon.vandenberghe@ugent.be>
2025-03-21 17:10:05 +0100

    Blindness has image now

--- src/main/java/be/ugent/objprog/minionwars/effects/BlindnessEffect.java
+++ src/main/java/be/ugent/objprog/minionwars/effects/BlindnessEffect.java
@@ -1,10 +1,15 @@
 package be.ugent.objprog.minionwars.effects;
 
 import be.ugent.objprog.minionwars.minions.Minion;
+import javafx.scene.image.Image;
+
+import java.util.Objects;
 
 public class BlindnessEffect extends MinionEffect {
+
     public BlindnessEffect(int baseDuration, int value) {
         super(baseDuration, value);
+        image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/be/ugent/objprog/minionwars/images/effects/blindness.png")));
     }
 
     @Override
